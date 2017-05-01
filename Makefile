71be6f8d730c2a9db5f84f7acb9db40e2a987ad4
Micah Chambers <micah@getcruise.com>
2017-05-01 14:08:21 -0700

    non-sparse bspline works

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 #CFLAGS="-mtune=generic"
 #CFLAGS=-std=c++11 -O3 -march=native -I/usr/include/eigen3 -Wno-deprecated-declarations
-CXX=clang++
-QTLIBS=-lQt53DCore -lQt53DExtras -lQt53DInput -lQt53DLogic -lQt53DQuickExtras -lQt53DQuickInput -lQt53DQuickRender -lQt53DQuick -lQt53DRender -lQt5Bluetooth -lQt5Charts -lQt5CLucene -lQt5Concurrent -lQt5Core -lQt5DataVisualization -lQt5DBus -lQt5DesignerComponents -lQt5Designer -lQt5EglFSDeviceIntegration -lQt5EglFsKmsSupport -lQt5Gamepad -lQt5Gui -lQt5Help -lQt5Location -lQt5MultimediaQuick_p -lQt5Multimedia -lQt5MultimediaWidgets -lQt5Network -lQt5Nfc -lQt5OpenGL -lQt5Positioning -lQt5PrintSupport -lQt5Qml -lQt5QuickControls2 -lQt5QuickParticles -lQt5Quick -lQt5QuickTemplates2 -lQt5QuickTest -lQt5QuickWidgets -lQt5Script -lQt5ScriptTools -lQt5Scxml -lQt5Sensors -lQt5SerialBus -lQt5SerialPort -lQt5Sql -lQt5Svg -lQt5Test -lQt5WaylandClient -lQt5WaylandCompositor -lQt5WebChannel -lQt5WebEngineCore -lQt5WebEngine -lQt5WebEngineWidgets -lQt5WebKit -lQt5WebKitWidgets -lQt5WebSockets -lQt5WebView -lQt5Widgets -lQt5X11Extras -lQt5XcbQpa -lQt5XmlPatterns -lQt5Xml
+#CXX=
 CFLAGS=-std=c++11 -ggdb -march=native -I/usr/include/eigen3 -Wno-deprecated-declarations
-CXX_FLAGS = -fPIC -O3 -Wall -w -std=c++11 -I/usr/include/eigen3 -I/usr/include/qt -I.
+CXX_FLAGS = -lspqr -fPIC -ggdb -O3 -Wall -w -std=c++11 -I/usr/include/eigen3 -I/usr/include/suitesparse/ -I.
 
 all: math array copy zero bspline graph sinc gradtest linesearch plottest \
 	bspline_regularize goldensearch struct_construct
@@ -45,8 +44,11 @@ extern: extern_1.cpp extern_1.h extern_2.cpp
 gzip: gzip.cpp
 	g++ gzip.cpp -o gzip -std=c++11 -lz
 
-bspline_fit: bspline_fit.cpp
-	${CXX} ${CXX_FLAGS} ${QTLIBS} $< -o $@
+bspline_fit: bspline_fit.o
+	${CXX} -lspqr $< -o $@
+
+bspline_fit.o: bspline_fit.cpp
+	${CXX} -c -fPIC -ggdb -O3 -Wall -w -std=c++11 -I/usr/include/eigen3 -I/usr/include/suitesparse/ -I. $< -o $@
 
 : gzip.cpp
 	g++ gzip.cpp -o gzip -std=c++11 -lz
--- bspline_fit.cpp
+++ bspline_fit.cpp
@@ -1,23 +1,22 @@
 #include <Eigen/Sparse>
+#include <Eigen/SPQRSupport>
+#include <Eigen/OrderingMethods>
+#include <Eigen/SparseQR>
+#include <Eigen/Dense>
+#include <iostream>
+#include <fstream>
 #include <vector>
-#include <QtGui/QImage>
 
 // declares a column-major sparse matrix type of double
 typedef Eigen::SparseMatrix<float> SparseMatrix;
 typedef Eigen::Triplet<float> Triplet;
 
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> RowMajorMatrixXf;
 
-void saveAsBitmap(const Eigen::VectorXf& x, size_t rows, size_t cols, const char* fname)
-{
-    Eigen::Array<unsigned char, Eigen::Dynamic, Eigen::Dynamic> bits = (x*255).cast<unsigned char>();
-    QImage img(bits.data(), rows, cols, QImage::Format_Indexed8);
-    img.setColorCount(256);
-
-    for(int i = 0; i < 256; i++) {
-        img.setColor(i,qRgb(i,i,i));
-    }
-    img.save(fname);
-}
+const float MIN_X = 0;
+const float MAX_X = 359.9999;
+const float MIN_Y = 30;
+const float MAX_Y = 150;
 
 template <typename T>
 T clamp(const T& v, const T& min_v, const T& max_v)
@@ -25,10 +24,25 @@ T clamp(const T& v, const T& min_v, const T& max_v)
     return v < min_v ? min_v : (v > max_v ? max_v : v);
 }
 
-inline
-int wrap(const int& v, const int& min_v, const int& max_v)
+void saveAsBitmap(const Eigen::VectorXf& x, size_t rows, size_t cols, const char* fname)
 {
-    return v < min_v ? max_v + 1 - v : (v > max_v ? v - max_v : v);
+    std::ofstream ofs(fname);
+    ofs << "P2\n" << cols << " " << rows << "\n255\n";
+    for(size_t rr = 0; rr < rows; rr++) {
+        for(size_t cc = 0; cc < cols; cc++) {
+            ofs << clamp<int>((int)x[rr * cols + cc], 0, 255) << " ";
+        }
+        ofs << "\n";
+    }
+}
+
+inline
+int wrap(int v, int min_v, int max_v)
+{
+    int width = max_v - min_v + 1;
+    while(v < min_v) v += width;
+    while(v > max_v) v -= width;
+    return v;
 }
 
 inline
@@ -78,12 +92,12 @@ size_t grid_index(size_t width, size_t height, size_t grid_x, size_t grid_y)
 
 struct BSpline
 {
-    float grid_dx = 0.1;
-    float grid_dy = 0.2;
-    float grid_min_x = 0;
-    float grid_min_y = 0;
-    float grid_max_x = 1;
-    float grid_max_y = 1;
+    float grid_dx = 5;
+    float grid_dy = 5;
+    float grid_min_x = MIN_Y;
+    float grid_max_x = MAX_X;
+    float grid_min_y = MIN_Y;
+    float grid_max_y = MAX_Y;
 
     size_t x_knots = (grid_max_x - grid_min_x) / grid_dx;
     size_t y_knots = (grid_max_y - grid_min_y) / grid_dy;
@@ -97,18 +111,21 @@ struct BSpline
 
         for(size_t pp = 0; pp < points.rows(); pp++) {
             const auto& pt = points.row(pp);
-            float c_xbin = (pt.x() - grid_min_x)/(grid_max_x - grid_min_x);
-            float c_ybin = (pt.y() - grid_min_y)/(grid_max_y - grid_min_y);
+            float c_xbin = x_knots * (pt.x() - grid_min_x)/(grid_max_x - grid_min_x);
+            float c_ybin = y_knots * (pt.y() - grid_min_y)/(grid_max_y - grid_min_y);
 
             // add weights to adjacent knots
             for(int ii = -2; ii <= 2; ii++) {
                 for(int jj = -2; jj <= 2; jj++) {
-                    int xbin = wrap(xbin + ii, 0, x_knots - 1);
-                    int ybin = clamp<int>(ybin + jj, 0, y_knots - 1);
-                    float w = kernel(pt.x() - (xbin + ii)) *
-                              kernel(pt.y() - (ybin + jj));
+                    int raw_xbin = (int)c_xbin + ii;
+                    int wrapped_xbin = wrap(raw_xbin, 0, x_knots - 1);
 
-                    size_t grid_ind = grid_index(x_knots, y_knots, xbin, ybin);
+                    int ybin = (int)c_ybin + jj;
+                    if(ybin < 0 || ybin >= y_knots)
+                        continue;
+
+                    float w = kernel(c_xbin - raw_xbin)*kernel(c_ybin - ybin);
+                    size_t grid_ind = grid_index(x_knots, y_knots, wrapped_xbin, ybin);
                     coeffs.push_back(Triplet(pp, grid_ind, w));
                 }
             }
@@ -126,8 +143,26 @@ Eigen::VectorXf solve(const BSpline& spl, const Eigen::MatrixXf& points)
 {
     auto Bmat = spl.getWeights(points);
 
-    Eigen::SimplicialCholesky<SparseMatrix> chol(Bmat);
-    return chol.solve(points.col(2));
+//    auto tmp = (RowMajorMatrixXf::Identity(Bmat.rows(), Bmat.rows()) * Bmat);
+//    for(size_t rr = 0; rr < Bmat.rows(); rr++) {
+//        RowMajorMatrixXf row = tmp.row(rr);
+//        Eigen::Map<RowMajorMatrixXf> rmap(row.data(), spl.y_knots, spl.x_knots);
+//        std::cerr << "\n-------\n" << rmap << "\n-----\n" << std::endl;
+//    }
+//    std::cerr << (RowMajorMatrixXf::Identity(Bmat.rows(), Bmat.rows()) * Bmat) << std::endl;
+
+    // make it dense :(
+    auto tmp = (RowMajorMatrixXf::Identity(Bmat.rows(), Bmat.rows()) * Bmat);
+    Eigen::ColPivHouseholderQR<Eigen::MatrixXf> qr(tmp);
+
+    // Right now this doesn't seem to work ...
+    // Bmat.makeCompressed();
+    //Eigen::SparseQR<SparseMatrix, Eigen::NaturalOrdering<int>> qr;
+    //Eigen::SparseQR<SparseMatrix, Eigen::COLAMDOrdering<int>> qr;
+    //Eigen::SPQR<SparseMatrix> qr;
+    //qr.compute(Bmat);
+    //Eigen::SparseLU<SparseMatrix> qr(Bmat);
+    return qr.solve(points.col(2));
 };
 
 Eigen::VectorXf interpolate(const BSpline& spl,
@@ -137,11 +172,34 @@ Eigen::VectorXf interpolate(const BSpline& spl,
     return bmat * knot_values;
 }
 
+void drawPoints(const BSpline& spl, const Eigen::MatrixXf& points, const char* fname)
+{
+    size_t x_bins = spl.x_knots * 10;
+    size_t y_bins = spl.y_knots * 10;
+    Eigen::VectorXf raster(x_bins * y_bins);
+    raster.fill(0);
+    for(size_t rr = 0; rr < points.rows(); rr++) {
+        float x = points(rr, 0);
+        float y = points(rr, 1);
+        float c_xbin = x_bins * (x - spl.grid_min_x) / (spl.grid_max_x - spl.grid_min_x);
+        float c_ybin = y_bins * (y - spl.grid_min_y) / (spl.grid_max_y - spl.grid_min_y);
+        raster[(int)c_xbin + (int)c_ybin * x_bins] = 255;
+    }
+    saveAsBitmap(raster, y_bins, x_bins, fname);
+}
+
 int main(int argc, char** argv)
 {
-    size_t num_points = 100;
-    auto points = Eigen::MatrixXf::Random(num_points, 3); // x, y, value
+    size_t num_points = 30;
+    Eigen::MatrixXf points = Eigen::MatrixXf::Random(num_points, 3); // x, y, value
+    for(size_t rr = 0; rr < points.rows(); rr++) {
+        points(rr, 0) = MIN_X + std::abs(points(rr, 0)) * (MAX_X - MIN_X);
+        points(rr, 1) = MIN_Y + std::abs(points(rr, 1)) * (MAX_Y - MIN_Y);
+        points(rr, 2) = 1;
+    }
+
     BSpline spl;
+    drawPoints(spl, points, "orig.pgm");
 
     // Matrix maps from knots to known points
     // v = Bx
@@ -152,16 +210,17 @@ int main(int argc, char** argv)
     Eigen::VectorXf knots = solve(spl, points);
 
     // Use the known knots to solve for the actual values at each knot location
-    Eigen::MatrixXf grid_points((spl.x_knots - 1)*(spl.y_knots - 1), 2);
+    Eigen::MatrixXf grid_points(spl.x_knots*spl.y_knots, 2);
     for(size_t yy = 0; yy < spl.y_knots; yy++) {
         for(size_t xx = 0; xx < spl.x_knots; xx++) {
-            grid_points(yy * spl.x_knots + xx, 0) = xx + 0.5;
-            grid_points(yy * spl.x_knots + xx, 1) = yy + 0.5;
+            grid_points(yy * spl.x_knots + xx, 0) = spl.grid_min_x + xx * spl.grid_dx;
+            grid_points(yy * spl.x_knots + xx, 1) = spl.grid_min_y + yy * spl.grid_dy;
         }
     }
 
     Eigen::VectorXf interpolated = interpolate(spl, knots, grid_points);
+    //Eigen::VectorXf interpolated = interpolate(spl, knots, points);
 
-    saveAsBitmap(interpolated, spl.x_knots - 1, spl.y_knots - 1, argv[1]);
+    saveAsBitmap(255*interpolated, spl.y_knots, spl.x_knots, "smoothed.pgm");
     return 0;
 }
